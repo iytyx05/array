85f5b5334e39d1b696b063671a643b718a0446f2
Islam Musabekov <islamchik3039@gmail.com>
2024-03-27 16:16:25 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// !Задание №1
+// let arr = ["dlrow", "olleH"];
+
+// arr = arr.map(a => a.split('').reverse().join(''));
+// arr.reverse();
+// console.log(arr); 
+
+
+// !Задание №2
+// let arr = ['а', 'б', 'в'];
+
+// arr.push('4, 3, 5, 5')
+// let arr2 = arr.join(',')
+// let arr3 = arr2.split(",")
+// console.log(arr3)
+
+// !Задание №3
+// let arr = ["Apple", "Orange", "Pinapple", "Cherry"];
+
+// let arr2 = arr.splice(0,1)
+// arr.splice(2,0, arr2[0])
+// console.log(arr)
+
+// !Задание №4
+// let arr = [18, -7, -21, 44, 31];
+
+// arr.sort((a, b) => b - a);
+// let arr2 = arr[0];
+// console.log("Наибольшее число в массиве:", arr2);
